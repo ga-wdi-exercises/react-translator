8b52db59188918d8c8c7e7c888773ccd79d009ad
Juan C Garcia <Juan.Garcia@generalassemb.ly>
2017-05-26 10:24:35 -0400

    voice starter

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -30,44 +30,14 @@ class Results extends Component {
     })
   }
 
-  getTextPronunciation() {
-    axios.get('https://watson-api-explorer.mybluemix.net/text-to-speech/api/v1/pronunciation', {
-      params: {
-        text: this.state.translation,
-        voice: this.state.selectedVoice
-      }
-    })
-    .then((response) => {
-      this.setState({
-        textPronunciation: response.data.pronunciation
-      })
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }
+  //TODO: Fetch Phonetic Pronunciation
 
-  getAudioPronunciation() {
-    this.setState({
-      audioPronunciationSource: `https://watson-api-explorer.mybluemix.net/text-to-speech/api/v1/synthesize?text=${this.state.translation}&voice=${this.state.selectedVoice}`
-    })
-  }
+  //TODO: Fetch audio
 
-  getPronunciations(e) {
-    e.preventDefault()
-    this.getTextPronunciation()
-    this.getAudioPronunciation()
-  }
   render() {
     let voices = this.state.voiceOptions.map((voice, index) => {
       return <option value={voice.name} key={index}>{voice.name}</option>
     })
-    let audio =
-      this.state.audioPronunciationSource?
-      <audio controls>
-        <source type="audio/ogg" src={this.state.audioPronunciationSource}/>
-      </audio> :
-      null
 
     return(
       <div>
@@ -75,15 +45,16 @@ class Results extends Component {
         <p>{this.state.translation}</p>
 
         <h3>Pronunciation:</h3>
-        <form onSubmit={(e) => this.getPronunciations(e)} >
+        <form>
           <p>Choose Voice</p>
           <select onChange={(e) => this.setVoice(e)}>
             {voices}
           </select>
           <input type="submit" value="Select Voice"/>
         </form>
-        <p>{this.state.textPronunciation}</p>
-        <p>{audio}</p>
+        <p><label>Selected Voice:</label>{ this.state.selectedVoice }</p>
+        <p><label>Phonetic Pronunciation:</label></p>
+        <p><label>Play Audio:</label></p>
       </div>
     )
   }
